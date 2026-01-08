180e1cad2041a3aa40448717ddea301c95e8a334
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2026-01-08 17:28:03 +0100

    Add read_project_file() with unit tests.

--- src/jobdata/table_runs/foreign_keys.rs
+++ src/jobdata/table_runs/foreign_keys.rs
@@ -6,9 +6,11 @@ use std::path::{Path, PathBuf};
 pub(crate) mod find_project_file;
 #[cfg(test)]
 pub(crate) mod project_mockup;
+#[cfg(test)]
+pub(crate) mod read_project_file;
 
 /// Struct to hold foreign key data for the runs table
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq)]
 pub struct RunsForeignKeys {
     pub project: String,
     pub code: String,
@@ -23,19 +25,34 @@ pub fn import_foreign_keys(file_name: &str, args: &CliArgs) -> Vec<String> {
     // Collect the SQL queries into a Vec<String> and process them later.
     let mut query_list: Vec<String> = Vec::new();
 
+    // Find a project file and read its RunsForeignKeys
+    let foreign_keys = read_project_file(file_name, args)
+        .unwrap_or_else(|e| panic!("Failed to read compulsory project file: {}", e));
+
     // Dummy usage to avoid unused variable warnings
-    let _dummy = find_project_file(file_name, args);
-    let _dummy2 = RunsForeignKeys {
-        project: String::new(),
-        code: String::new(),
-        code_version: String::new(),
-        test_case: String::new(),
-        cluster: None,
-        person: None,
-    };
+    let _dummy: &RunsForeignKeys = &foreign_keys;
 
     query_list.push("-- Inserting foreign keys into runs table;".to_string());
     query_list
+}
+
+/// Reads and parses the project file to extract RunsForeignKeys.
+/// Returns a RunsForeignKeys struct if successful, or an io::Error if there are issues
+/// reading or parsing the file.
+pub fn read_project_file(file_name: &str, args: &CliArgs) -> Result<RunsForeignKeys, io::Error> {
+    let project_file_path = find_project_file(file_name, args)?;
+    let file_contents = std::fs::read_to_string(&project_file_path)?;
+    let runs_foreign_keys: RunsForeignKeys = serde_yaml::from_str(&file_contents).map_err(|e| {
+        io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "Failed to parse project file '{}': {}",
+                project_file_path.display(),
+                e
+            ),
+        )
+    })?;
+    Ok(runs_foreign_keys)
 }
 
 /// Finds the project file by searching up the directory tree from the given file's location.
---
+++ src/jobdata/table_runs/foreign_keys/read_project_file.rs
@@ -0,0 +1,137 @@
+#[cfg(test)]
+mod tests {
+    use crate::cmdline::CliArgs;
+    use crate::jobdata::table_runs::foreign_keys::project_mockup::{
+        setup_cliargs_with_project_file_name, setup_tmp_project_file, teardown_tmp_project_file,
+    };
+    use crate::jobdata::table_runs::foreign_keys::read_project_file;
+    use crate::jobdata::table_runs::foreign_keys::RunsForeignKeys;
+
+    #[test]
+    fn test_read_project_file_success() {
+        // Create a temporary project file for testing
+        let args = CliArgs {
+            project_file: "test_project_file.yml".to_string(),
+            verbose: true,
+            dry_run: false,
+            ..Default::default()
+        };
+        let project_contents = RunsForeignKeys {
+            project: "TestProject".to_string(),
+            code: "TestCode".to_string(),
+            code_version: "1.0".to_string(),
+            test_case: "TestCase".to_string(),
+            cluster: Some("TestCluster".to_string()),
+            person: Some("TestPerson".to_string()),
+        };
+        let project_file = setup_tmp_project_file(&args, &project_contents);
+
+        // Create a file name in a subdirectory by replacing "test_project_file.yml"
+        // with "subdir1/subdir2/subdir3/LMX_summary.yml"
+        let subdir_file_name = project_file.replace(
+            "test_project_file.yml",
+            "subdir1/subdir2/subdir3/LMX_summary.yml",
+        );
+        std::fs::create_dir_all(std::path::Path::new(&subdir_file_name).parent().unwrap())
+            .expect("Failed to create subdirectory");
+        // the file may not exist, we only care for the directories to exist for the search
+        let result = read_project_file(&subdir_file_name, &args);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), project_contents);
+        // Clean up
+        teardown_tmp_project_file(&project_file);
+    }
+
+    #[test]
+    fn test_read_project_file_not_found() {
+        // Create command line arguments for testing
+        let args = CliArgs {
+            project_file: "non_existent_project_file.yml".to_string(),
+            verbose: true,
+            dry_run: false,
+            ..Default::default()
+        };
+        let file_name = "some_file.yml";
+        let result = read_project_file(file_name, &args);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_read_project_file_invalid_yaml() {
+        // Create a temporary project file with invalid YAML content
+        let temp_dir =
+            std::env::temp_dir().join(format!("project_file_test_{}", uuid::Uuid::new_v4()));
+        let project_file = temp_dir
+            .join("absolute_project_file.yml")
+            .to_str()
+            .unwrap()
+            .to_string();
+        // Create CliArgs with the specified project file
+        // This also sets up the necessary directories
+        let args = setup_cliargs_with_project_file_name(&project_file);
+        let invalid_yaml_content = "invalid_yaml: [unclosed_list";
+        std::fs::write(&project_file, invalid_yaml_content)
+            .expect("Failed to write invalid YAML to project file");
+        let result = read_project_file("some/random/path/LMX_summary.yml", &args);
+        assert!(result.is_err());
+        // Clean up
+        teardown_tmp_project_file(&project_file);
+    }
+
+    #[test]
+    fn test_read_project_file_incomplete_yaml() {
+        // Create a temporary project file with invalid YAML content
+        let temp_dir =
+            std::env::temp_dir().join(format!("project_file_test_{}", uuid::Uuid::new_v4()));
+        let project_file = temp_dir
+            .join("absolute_project_file.yml")
+            .to_str()
+            .unwrap()
+            .to_string();
+        // Create CliArgs with the specified project file
+        // This also sets up the necessary directories
+        let args = setup_cliargs_with_project_file_name(&project_file);
+        let invalid_yaml_content = format!(
+            "---\n{}\n{}\n{}\n",
+            "project: TestProject", "code: TestCode", "code_version: 1.0"
+        );
+        std::fs::write(&project_file, invalid_yaml_content)
+            .expect("Failed to write invalid YAML to project file");
+        let result = read_project_file("some/random/path/LMX_summary.yml", &args);
+        assert!(result.is_err());
+        // Clean up
+        teardown_tmp_project_file(&project_file);
+    }
+
+    #[test]
+    fn test_read_project_file_complete_project() {
+        // Create a temporary project file with invalid YAML content
+        let temp_dir =
+            std::env::temp_dir().join(format!("project_file_test_{}", uuid::Uuid::new_v4()));
+        let project_file = temp_dir
+            .join("absolute_project_file.yml")
+            .to_str()
+            .unwrap()
+            .to_string();
+        // Create CliArgs with the specified project file
+        // This also sets up the necessary directories
+        let args = setup_cliargs_with_project_file_name(&project_file);
+        let invalid_yaml_content = format!(
+            "---\n{}\n{}\n{}\n{}\n",
+            "project: TestProject", "code: TestCode", "code_version: 1.0", "test_case: TestCase"
+        );
+        std::fs::write(&project_file, invalid_yaml_content)
+            .expect("Failed to write invalid YAML to project file");
+        let result = read_project_file("some/random/path/LMX_summary.yml", &args);
+        assert!(result.is_ok());
+        let runs_foreign_keys = result.unwrap();
+        assert_eq!(runs_foreign_keys.project, "TestProject");
+        assert_eq!(runs_foreign_keys.code, "TestCode");
+        assert_eq!(runs_foreign_keys.code_version, "1.0");
+        assert_eq!(runs_foreign_keys.test_case, "TestCase");
+        assert!(runs_foreign_keys.cluster.is_none());
+        assert!(runs_foreign_keys.person.is_none());
+        // Clean up
+        teardown_tmp_project_file(&project_file);
+    }
+}
