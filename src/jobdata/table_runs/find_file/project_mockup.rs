0f531434e79a1aeddc212c140ba4e4acf002208c
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2026-01-22 17:24:13 +0100

    List  project_mockup.rs as #[cfg(test)].

--- src/jobdata/table_runs/find_file/project_mockup.rs
+++ src/jobdata/table_runs/find_file/project_mockup.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(test)]
 use crate::cmdline::CliArgs;
 use crate::jobdata::process_lmx_file;
 use crate::jobdata::table_runs::foreign_keys::RunsForeignKeys;
